cd033be5166c2e69607c86c6adf93c20e2e89b0a
s3anAK <56090826+s3anAK@users.noreply.github.com>
2022-05-20 16:58:22 -0700

    Created date parser to format the links to recreation.gov but some work is still required to fix bugs

--- checker.py
+++ checker.py
@@ -1,13 +1,28 @@
 import requests
-from datetime import datetime
+import datetime
+from date_arg_parser import date_parser
+import calendar
 
 #api_key = input("Enter your api key")
 
 permit_area= input("Please enter your desired area. Supported options include 'Inyo National Forest' and 'SEKI National Park' ")
 permit_codes = {'Inyo National Forest': 233262, "SEKI National Park": 445857}
-starting_date= input("Please enter your desired starting date in the format yyyy-mm-dd")
-ending_date= input("Please enter your desired ending date in the format yyyy-mm-dd")
+starting_date_input= input("Please enter your desired starting date in the format yyyy-mm-dd")
+ending_date_input= input("Please enter your desired ending date in the format yyyy-mm-dd")
+url_list =[]
 
+date=date_parser(starting_date_input,ending_date_input)
+date.month_and_date_parser()
+date.start_of_month()
+date.date_formatter()
 
-recreation_url = ("https://www.recreation.gov/api/permitinyo/" + str(permit_codes[permit_area]) + "/availability?start_date=2022-05-01&end_date=2022-05-31&commercial_acct=false")
-print(recreation_url)
+for i in date.months:
+    ending_date = datetime.date(i.year,i.month,calendar._monthlen(i.year,i.month))
+    temp_url = ("https://www.recreation.gov/api/permitinyo/" + str(permit_codes[permit_area]) + "/availability?start_date=" + str(i) + "&end_date=" + str(ending_date) + "&commercial_acct=false")
+    url_list.append(temp_url)
+    #print(temp_url)
+
+print(url_list)
+
+#recreation_url = ("https://www.recreation.gov/api/permitinyo/" + str(permit_codes[permit_area]) + "/availability?start_date=2022-05-01&end_date=2022-05-31&commercial_acct=false")
+#print(recreation_url)
