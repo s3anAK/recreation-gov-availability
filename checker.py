093007b956fe3064b58ea2a43187094383bfd8d0
s3anAK <56090826+s3anAK@users.noreply.github.com>
2022-07-24 18:56:43 -0700

    Fixed date template and added functionality to search for permit entrance ID

--- checker.py
+++ checker.py
@@ -2,13 +2,17 @@ import requests
 import datetime
 from date_arg_parser import date_parser
 import calendar
+import json
 
-#api_key = input("Enter your api key")
+api_key = 'ab3f1f1a-9cc2-4c20-bff6-dbf3a2d9fa96'
 
-permit_area= input("Please enter your desired area. Supported options include 'Inyo National Forest' and 'SEKI National Park' ")
+#permit_area= input("Please enter your desired area. Supported options include 'Inyo National Forest' and 'SEKI National Park' ")
+permit_area = "Inyo National Forest"
 permit_codes = {'Inyo National Forest': 233262, "SEKI National Park": 445857}
-starting_date_input= input("Please enter your desired starting date in the format yyyy-mm-dd")
-ending_date_input= input("Please enter your desired ending date in the format yyyy-mm-dd")
+#starting_date_input= input("Please enter your desired starting date in the format yyyy-mm-dd ")
+starting_date_input='2022-08-03'
+#ending_date_input= input("Please enter your desired ending date in the format yyyy-mm-dd ")
+ending_date_input="2022-08-21"
 url_list =[]
 
 date=date_parser(starting_date_input,ending_date_input)
@@ -20,9 +24,16 @@ for i in date.months:
     ending_date = datetime.date(i.year,i.month,calendar._monthlen(i.year,i.month))
     temp_url = ("https://www.recreation.gov/api/permitinyo/" + str(permit_codes[permit_area]) + "/availability?start_date=" + str(i) + "&end_date=" + str(ending_date) + "&commercial_acct=false")
     url_list.append(temp_url)
-    #print(temp_url)
 
 print(url_list)
 
-#recreation_url = ("https://www.recreation.gov/api/permitinyo/" + str(permit_codes[permit_area]) + "/availability?start_date=2022-05-01&end_date=2022-05-31&commercial_acct=false")
-#print(recreation_url)
+test_entrance = 'Baxter Pass'
+entrance_url = 'https://ridb.recreation.gov/api/v1/facilities/' + str(permit_codes[permit_area]) + '/permitentrances'
+payload = {'query': test_entrance}
+headers = {'apikey': api_key}
+results = requests.get(entrance_url,headers=headers, params=payload)
+results = json.loads(results.text)
+permit_entrance_id = (results['RECDATA'][0]['PermitEntranceID'])
+print(permit_entrance_id)
+
+
--- date_arg_parser.py
+++ date_arg_parser.py
@@ -1,48 +1,39 @@
 import datetime
 from dateutil import rrule
-import calendar
+import re
 
 class date_parser:
     def __init__(self, start_date, end_date):
         self.start_date = start_date
         self.end_date = end_date
         self.months = ''
+        self.dates = ''
+
+    def month_and_date_parser(self):
+        self.dates=[self.start_date.split('-'),self.end_date.split('-')]
+        count1 = 0
+        count2 = 0
+        for date in self.dates:
+            for part in date:
+                part = re.sub('^0','',part)
+                self.dates[count1][count2] = part
+                count2 += 1
+            count1 += 1
+            count2=0
 
     def start_of_month(self):
-        self.start_date = datetime.date(int(self.start_date[0:4]), int(self.start_date[5:6]), 1)
+        self.start_date = datetime.date(int(self.dates[0][0]), int(self.dates[0][1]), 1)
         print(self.start_date)
-        self.end_date = datetime.date(int(self.end_date[0:4]), int(self.end_date[5:6]), int(self.end_date[8:9]))
+        self.end_date = datetime.date(int(self.dates[1][0]), int(self.dates[1][1]), int(self.dates[1][2]))
         print(self.end_date)
         self.months = list(rrule.rrule(rrule.MONTHLY, dtstart=self.start_date, until=self.end_date))
         print(self.months)
-
-
-    def month_and_date_parser(self):
-        if int(self.start_date[5]) == 0:
-            self.start_date= self.start_date[:5] + self.start_date[6:]
-        if int(self.end_date[5]) == 0:
-            self.end_date= self.end_date[:5] + self.end_date[6:]
-        if int(self.start_date[8]) == 0:
-            self.start_date = self.start_date[:8] + self.start_date[9:]
-        if int(self.end_date[8]) == 0:
-            self.end_date = self.end_date[:8] + self.end_date[9:]
 
     def date_formatter(self):
         counter = 0
         for i in self.months:
             self.months[counter] = datetime.date(i.year,i.month,i.day)
             counter += 1
-
-'''
-    def end_dates(self):
-        print(self.months)
-        counter = 0
-        for i in self.months:
-            print(i)
-            self.months.insert(counter+1, datetime(i.year, i.month, calendar._monthlen(i.year, i.month)))
-            counter += 1
-            print(self.months)
-'''
 
 
 
