8432cbc4afbf04250b35940682932cd027ef3f0f
s3anAK <56090826+s3anAK@users.noreply.github.com>
2022-08-29 10:23:18 -0700

    Fixed typo on availability

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/s3anAK/recreation-gov-availibility.git
-cd $(find ~/ -type d -name "recreation-gov-availibility")
+git clone https://github.com/s3anAK/recreation-gov-availability.git
+cd $(find ~/ -type d -name "recreation-gov-availability")
 rm -rf .git
 cp example.secrets.py secrets.py
 sed -i -E "s/''/'$1'/g" secrets.py
