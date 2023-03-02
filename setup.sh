73b395148cd888f68d6a52d6c82d05a87f267f11
Scott Barnes <scottreidbarnes@gmail.com>
2023-03-01 22:38:36 -0800

    Move secrets to environment variables and update README to reflect this
    Also:
    - Make it easier for users to use their own Gmail account.
    - Bump fake-useragent version
    - Prompt user to enter credentials after running setup.sh
    - Prompt the user to enter credentials after running setup.sh
    - Check if the email in trips.json is placeholder@gmail.com and alert the user.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,37 @@
 cd $(find ~/ -type d -name "recreation-gov-availability")
 rm -rf .git
-cp example.secrets.py secrets.py
-sed -i -E "s/''/'$1'/g" secrets.py
 virtualenv .
 source bin/activate
 pip install -r requirements.txt
+
+# Populate .env last so it's the last output on the screen.
+if [[ ! -e .env ]]; then
+  printf "\n"
+  read -p "Enter the Gmail account to send notifications from: " FROM_GMAIL
+  read -p "Enter the associated Gmail App Password: " FROM_GMAIL_APP_PASSWORD
+  read -p "Enter your ridb.recreation.gov API key: " API_KEY
+
+  printf "\n"
+  if [[ ! -z "$FROM_GMAIL" ]]; then
+    echo "FROM_GMAIL=$FROM_GMAIL" >> .env
+  else
+    echo "No Gmail entered. See README.md and update .env manually"
+  fi
+
+  if [[ ! -z "$FROM_GMAIL_APP_PASSWORD" ]]; then
+    echo "FROM_GMAIL_APP_PASSWORD=$FROM_GMAIL_APP_PASSWORD" >> .env
+  else
+    echo "No Gmail App Password entered. See README.md and update .env manually"
+  fi
+
+  if [[ ! -z "$API_KEY" ]]; then
+    echo "API_KEY=$API_KEY" >> .env
+  else
+    echo "No API key entered. See README.md and update .env manually"
+  fi
+else
+  printf "Skipping .env configuration as .env already exists.\n\n"
+fi
+
 deactivate
 chmod +x permit.sh
